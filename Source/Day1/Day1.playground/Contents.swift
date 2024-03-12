a80c876e61f47d4d011cfc767d2a29979021d1e2
Patrick <patrick.wittybunny@gmail.com>
2024-03-12 15:58:54 +0800

    Day 1 - Beginner Challenge Problems (#1)

---
+++ Source/Day1/Day1.playground/Contents.swift
@@ -0,0 +1,133 @@
+import UIKit
+
+class Solution {
+    /// simple
+    static func runningSum(_ nums: [Int]) -> [Int] {
+        var sum = 0
+        var running = [Int]()
+        for num in nums {
+            sum += num
+            running.append(sum)
+        }
+        return running
+    }
+    
+    /// functional
+    static func runningSum2(_ nums: [Int]) -> [Int] {
+        return nums.reduce(into: []) { newArr, num in
+            newArr.append(num + (newArr.last ?? 0))
+        }
+    }
+    
+    /// simple
+    static func maximumWealth(_ accounts: [[Int]]) -> Int {
+        return accounts.map { $0.reduce(0, +) }.max()!
+    }
+    
+    /// simple
+    static func fizzBuzz(_ n: Int) -> [String] {
+        (1...n).map {
+            if $0.isMultiple(of: 15) {
+                return "FizzBuzz"
+            } else if $0.isMultiple(of: 3) {
+                return "Fizz"
+            } else if $0.isMultiple(of: 5) {
+                return "Buzz"
+            }
+            return "\($0)"
+        }
+    }
+    
+    /// simple
+    static func numberOfSteps(_ num: Int) -> Int {
+        var steps = 0
+        var running = num
+        while running > 0 {
+            if running % 2 == 0 { running /= 2 }
+            else { running -= 1}
+            steps += 1
+        }
+        return steps
+    }
+    
+    /// bitwise
+    static func numberOfSteps2(_ num: Int) -> Int {
+        if num == 0 { return 0 }
+        return num.bitWidth - num.leadingZeroBitCount + num.nonzeroBitCount - 1
+    }
+    
+    public class ListNode {
+        public var val: Int
+        public var next: ListNode?
+        public init() { self.val = 0; self.next = nil; }
+        public init(_ val: Int) { self.val = val; self.next = nil; }
+        public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    }
+    
+    // simple
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var length = 0
+        var node: ListNode? = head
+        while node != nil {
+            node = node?.next
+            length += 1
+        }
+        
+        length = (length / 2) + 1
+        
+        node = head
+        for _ in 0..<length {
+            node = node?.next
+        }
+        
+        return node
+    }
+    
+    // fast, slow
+    func middleNode2(_ head: ListNode?) -> ListNode? {
+        var fast: ListNode? = head
+        var slow: ListNode? = head
+        
+        while fast?.next != nil {
+            fast = fast?.next?.next
+            slow = slow?.next
+        }
+        
+        return slow
+    }
+    
+    static func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        if magazine.count < ransomNote.count { return false }
+        if magazine.count == 1, ransomNote.count == 1 { return magazine == ransomNote }
+        var ransomMap = [Character: Int]()
+        for char in ransomNote {
+            let value = ransomMap[char, default: 0]
+            ransomMap[char] = value + 1
+        }
+        for char in magazine {
+            let value = ransomMap[char] ?? 0
+            if (value - 1) <= 0 {
+                ransomMap.removeValue(forKey: char)
+            } else {
+                ransomMap[char] = value - 1
+            }
+            
+            if ransomMap.isEmpty {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+print(Solution.runningSum([1,2,3,4]))
+print(Solution.runningSum2([1,2,3,4]))
+print(Solution.runningSum([5,10,35,50]))
+print(Solution.runningSum2([5,10,35,50]))
+print(Solution.maximumWealth([[1,2,3],[3,2,1]]))
+print(Solution.fizzBuzz(15))
+print(Solution.numberOfSteps(14))
+print(Solution.numberOfSteps2(14))
+print(Solution.canConstruct("a", "b"))
+print(Solution.canConstruct("aa", "ab"))
+print(Solution.canConstruct("aa", "aab"))
---
+++ Source/Day1/README.md
@@ -0,0 +1,75 @@
+# Problems
+
+[Ref](https://leetcode.com/explore/learn/card/the-leetcode-beginners-guide/679/sql-syntax/4357/)
+
+## 1. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+> Input: nums = [1,2,3,4]
+> Output: [1,3,6,10]
+> Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+## 2. Richest Customer Wealth
+
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+> Input: accounts = [[1,2,3],[3,2,1]]
+> Output: 6
+> Explanation:
+> 1st customer has wealth = 1 + 2 + 3 = 6
+> 2nd customer has wealth = 3 + 2 + 1 = 6
+> Both customers are considered the richest with a wealth of 6 each, so return 6.
+
+## 3. Fizz Buzz
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+* answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+* answer[i] == "Fizz" if i is divisible by 3.
+* answer[i] == "Buzz" if i is divisible by 5.
+* answer[i] == i (as a string) if none of the above conditions are true.
+
+> Input: n = 3
+> Output: ["1","2","Fizz"]
+
+
+## 4. Number of Steps to Reduce a Number to Zero
+
+Given an integer num, return the number of steps to reduce it to zero.
+
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+> Input: num = 14
+> Output: 6
+> Explanation: 
+> Step 1) 14 is even; divide by 2 and obtain 7. 
+> Step 2) 7 is odd; subtract 1 and obtain 6.
+> Step 3) 6 is even; divide by 2 and obtain 3. 
+> Step 4) 3 is odd; subtract 1 and obtain 2. 
+> Step 5) 2 is even; divide by 2 and obtain 1. 
+> Step 6) 1 is odd; subtract 1 and obtain 0.
+
+## 5. Middle of the Linked List
+
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+> Input: head = [1,2,3,4,5]
+> Output: [3,4,5]
+> Explanation: The middle node of the list is node 3.
+
+## 6. Ransom Note
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+> Input: ransomNote = "a", magazine = "b"
+> Output: false
+
