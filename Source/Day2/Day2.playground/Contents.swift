32edfa5b4c0fdc49c975364c801c050fdb847d7b
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-13 12:03:56 +0800

    - squares of sorted array

--- Source/Day2/Day2.playground/Contents.swift
+++ Source/Day2/Day2.playground/Contents.swift
@@ -18,7 +18,36 @@ class Solution {
         return nums.filter { String($0).count % 2 == 0 }.count
     }
     
+    // simple
+    static func sortedSquares(_ nums: [Int]) -> [Int] {
+        return nums.map { $0 * $0 }.sorted()
+    }
+    
+    // optimized
+    static func sortedSquares2(_ nums: [Int]) -> [Int] {
+        var right = nums.count - 1
+        var left = 0
+        
+        var result = nums
+        var index = right
+
+        while index >= 0 {
+            if abs(nums[left]) < abs(nums[right]) {
+                result[index] = nums[right] * nums[right]
+                right -= 1
+            } else {
+                result[index] = nums[left] * nums[left]
+                left += 1
+            }
+            index -= 1
+        }
+        
+        return result
+    }
+    
 }
 
 print(Solution.findMaxConsecutiveOnes([1,1,0,1,1,1]))
 print(Solution.findNumbers([555,901,482,1771]))
+print(Solution.sortedSquares([-7,-3,2,3,11]))
+print(Solution.sortedSquares2([-7,-3,2,3,11]))
--- Source/Day2/README.md
+++ Source/Day2/README.md
@@ -25,3 +25,14 @@ Explanation:
 7896 contains 4 digits (even number of digits). 
 Therefore only 12 and 7896 contain an even number of digits.
 ```
+
+## 3. Squares of a Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+```
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+```
