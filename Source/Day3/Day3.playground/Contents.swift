f95d2878ea7d76218124f56f2c692ebf5be8ba77
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-14 10:48:36 +0800

    - remove duplicates

--- Source/Day3/Day3.playground/Contents.swift
+++ Source/Day3/Day3.playground/Contents.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 class Solution {
     // simple
     static func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
@@ -16,6 +18,26 @@ class Solution {
         }
         return count
     }
+    
+    // simple
+    static func removeDuplicates(_ nums: inout [Int]) -> Int {
+        nums = NSOrderedSet(array: nums).array as! [Int]
+        return nums.count
+    }
+    
+    // pointers
+    static func removeDuplicates2(_ nums: inout [Int]) -> Int {
+        var count = 0
+        var unique = Int.min
+        for index in 0..<nums.count {
+            if unique != nums[index] {
+                unique = nums[index]
+                nums.swapAt(index, count)
+                count += 1
+            }
+        }
+        return count
+    }
 }
 
 
@@ -28,3 +50,11 @@ var array2 = [0,1,2,2,3,0,4,2]
 Solution.removeElement2(&array2, 2)
 print(array2)
 print(array2.count)
+
+var array3 = [0,0,1,1,1,2,2,3,3,4]
+print(Solution.removeDuplicates(&array3))
+print(array3)
+
+var array4 = [0,0,1,1,1,2,2,3,3,4]
+print(Solution.removeDuplicates2(&array4))
+print(array4)
