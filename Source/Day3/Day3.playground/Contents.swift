d40e6a0303b82108eb6c17ba889c9dccfcea9a4e
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-14 10:31:50 +0800

    - remove element

--- Source/Day3/Day3.playground/Contents.swift
+++ Source/Day3/Day3.playground/Contents.swift
@@ -1,3 +1,30 @@
 class Solution {
+    // simple
+    static func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        nums.removeAll(where: { $0 == val })
+        return nums.count
+    }
     
+    //pointers
+    static func removeElement2(_ nums: inout [Int], _ val: Int) -> Int {
+        var count = 0
+        for index in 0..<nums.count {
+            if nums[index] != val {
+                nums.swapAt(index, count)
+                count += 1
+            }
+        }
+        return count
+    }
 }
+
+
+var array = [0,1,2,2,3,0,4,2]
+Solution.removeElement(&array, 2)
+print(array)
+print(array.count)
+
+var array2 = [0,1,2,2,3,0,4,2]
+Solution.removeElement2(&array2, 2)
+print(array2)
+print(array2.count)
