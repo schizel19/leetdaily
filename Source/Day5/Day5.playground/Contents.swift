387990205957aad1b417de06c19eed72e0e59b80
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-03-16 13:16:05 +0800

    - third maximum number

--- Source/Day5/Day5.playground/Contents.swift
+++ Source/Day5/Day5.playground/Contents.swift
@@ -28,6 +28,57 @@ class Solution {
         
         return notInPlace
     }
+    
+    // 3 loop
+    static func thirdMax(_ nums: [Int]) -> Int {
+        if nums.count == 1 { return nums[0] }
+        if nums.count == 2 { return max(nums[0], nums[1]) }
+        
+        var firstMax: Int!
+        var secondMax: Int!
+        var thirdMax: Int!
+        
+        for num in nums {
+            firstMax = firstMax ?? num
+            firstMax = max(num, firstMax)
+        }
+        
+        for num in nums {
+            guard num != firstMax else { continue }
+            secondMax = secondMax ?? num
+            secondMax = max(num, secondMax)
+        }
+        
+        for num in nums {
+            guard num != secondMax, num != firstMax else { continue }
+            thirdMax = thirdMax ?? num
+            thirdMax = max(num, thirdMax)
+        }
+        
+        return thirdMax != nil ? thirdMax! : firstMax
+    }
+    
+    // one loop
+    static func thirdMax2(_ nums: [Int]) -> Int {
+        var max1 = Int.min
+        var max2 = Int.min
+        var max3 = Int.min
+        
+        for num in nums {
+            if num > max1 {
+                max3 = max2
+                max2 = max1
+                max1 = num
+            } else if num > max2, num != max1 {
+                max3 = max2
+                max2 = num
+            } else if num > max3, num != max2, num != max1 {
+                max3 = num
+            }
+        }
+        
+        return max3 == Int.min ? max1 : max3
+    }
 }
 
 
@@ -38,3 +89,12 @@ print(Solution.heightChecker([1,2,3,4,5]))
 print(Solution.heightChecker2([1,1,4,2,1,3]))
 print(Solution.heightChecker2([5,1,2,3,4]))
 print(Solution.heightChecker2([1,2,3,4,5]))
+
+print(Solution.thirdMax([3,2,1]))
+print(Solution.thirdMax([2,1]))
+print(Solution.thirdMax([2,3,2,1]))
+
+
+print(Solution.thirdMax2([3,2,1]))
+print(Solution.thirdMax2([2,1]))
+print(Solution.thirdMax2([2,3,2,1]))
