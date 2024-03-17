0789f2ef1eed237ff7c9b2387ceab2e5c9fd3e0a
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-03-17 20:47:14 +0800

    - spiral order

--- Source/Day6/Day6.playground/Contents.swift
+++ Source/Day6/Day6.playground/Contents.swift
@@ -111,16 +111,49 @@ class Solution {
             
         return result
     }
+    
+    static func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        guard  !matrix.isEmpty else {
+            return []
+        }
+        var top = 0
+        var bottom = matrix.count - 1
+        var left = 0
+        var right = matrix[0].count - 1
+        let count = matrix.count * matrix[0].count
+        var arr = [Int]()
+        while arr.count < count {
+            for i in stride(from: left, to: right+1, by: 1) where arr.count < count {
+                arr.append(matrix[top][i])
+            }
+            top += 1
+            for i in stride(from: top, to: bottom+1, by: 1) where arr.count < count {
+                arr.append(matrix[i][right])
+            }
+            right -= 1
+            for i in stride(from: right, to: left-1, by: -1) where arr.count < count {
+                arr.append(matrix[bottom][i])
+            }
+            bottom -= 1
+            for i in stride(from: bottom, to: top-1, by: -1) where arr.count < count {
+                arr.append(matrix[i][left])
+            }
+            left += 1
+        }
+        return arr
+    }
 }
 
-//print(Solution.pivotIndex([1,7,3,6,5,6]))
-//print(Solution.pivotIndex([1,2,3]))
-//print(Solution.pivotIndex([2,1,-1]))
-//print(Solution.pivotIndex([0]))
-//print(Solution.dominantIndex([3,6,1,0]))
-//print(Solution.dominantIndex([1,2,3,4]))
-//print(Solution.plusOne([9]))
-//print(Solution.plusOne([4,3,2,1]))
-//print(Solution.plusOne([1,2,3]))
+print(Solution.pivotIndex([1,7,3,6,5,6]))
+print(Solution.pivotIndex([1,2,3]))
+print(Solution.pivotIndex([2,1,-1]))
+print(Solution.pivotIndex([0]))
+print(Solution.dominantIndex([3,6,1,0]))
+print(Solution.dominantIndex([1,2,3,4]))
+print(Solution.plusOne([9]))
+print(Solution.plusOne([4,3,2,1]))
+print(Solution.plusOne([1,2,3]))
 print(Solution.findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
 print(Solution.findDiagonalOrder([[1,2],[3,4]]))
+print(Solution.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(Solution.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
