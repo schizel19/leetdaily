673541bcbf0c7b627bbc0d8567c64a6171615e4a
Patrick <patrickleinieldomingo@gmail.com>
2024-03-17 21:01:10 +0800

    Day 6 - Arrays and String: Introduction (pt. 1) (#6)

---
+++ Source/Day6/Day6.playground/Contents.swift
@@ -0,0 +1,180 @@
+class Solution {
+    
+    // sol 1
+    static func pivotIndex(_ nums: [Int]) -> Int {
+        var sumOfLeft = [0]
+        var sumOfRight = [0]
+        
+        for index in 1..<nums.count {
+            sumOfLeft.append(sumOfLeft[index - 1] + nums[index - 1])
+            sumOfRight.insert(sumOfRight[0] + nums[nums.count - index], at: 0)
+        }
+        
+        var foundIndex = -1
+        
+        for (index, value) in sumOfLeft.enumerated() {
+            if sumOfRight[index] == value {
+                foundIndex = index
+                break
+            }
+        }
+        
+        return foundIndex
+    }
+    
+    // sol 2
+    static func pivotIndex2(_ nums: [Int]) -> Int {
+        var left = 0
+        var right = nums.reduce(0, +)
+        
+        for (index, num) in nums.enumerated() {
+            right -= num
+            if left == right {
+                return index
+            }
+            left += num
+        }
+        
+        return -1
+    }
+    
+    static func dominantIndex(_ nums: [Int]) -> Int {
+        let max = nums.enumerated().max { a, b in
+            a.element < b.element
+        }
+        
+        for (index, num) in nums.enumerated() {
+            if index == max!.offset { continue }
+            if num * 2 > max!.element {
+                return -1
+            }
+        }
+        
+        return max!.offset
+    }
+    
+    static func plusOne(_ digits: [Int]) -> [Int] {
+        var carry = 1
+        var digits = digits
+        
+        for index in stride(from: digits.count - 1, through: 0, by: -1) {
+            let runningSum = digits[index] + carry
+            digits[index] = runningSum % 10
+            carry = runningSum / 10
+            if carry == 0 {
+                break
+            }
+        }
+        
+        if carry == 1 {
+            digits.insert(1, at: 0)
+        }
+        
+        return digits
+    }
+    
+    static func findDiagonalOrder(_ mat: [[Int]]) -> [Int] {
+        var result = [Int]()
+        var i = 0
+        var j = 0
+        let n = mat.count
+        let m = mat[0].count
+        
+        var direction: Bool = true // up
+        while i < n, j < m {
+            result.append(mat[i][j])
+            
+            if direction {
+                if j + 1 == m {
+                    direction = false
+                    i += 1
+                } else if i == 0 {
+                    direction = false
+                    j += 1
+                } else {
+                    j += 1
+                    i -= 1
+                }
+            } else {
+                if i + 1 == n {
+                    direction = true
+                    j += 1
+                } else if j == 0 {
+                    direction = true
+                    i += 1
+                } else {
+                    i += 1
+                    j -= 1
+                }
+            }
+        }
+            
+        return result
+    }
+    
+    static func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        guard  !matrix.isEmpty else {
+            return []
+        }
+        var top = 0
+        var bottom = matrix.count - 1
+        var left = 0
+        var right = matrix[0].count - 1
+        let count = matrix.count * matrix[0].count
+        var arr = [Int]()
+        while arr.count < count {
+            for i in stride(from: left, to: right+1, by: 1) where arr.count < count {
+                arr.append(matrix[top][i])
+            }
+            top += 1
+            for i in stride(from: top, to: bottom+1, by: 1) where arr.count < count {
+                arr.append(matrix[i][right])
+            }
+            right -= 1
+            for i in stride(from: right, to: left-1, by: -1) where arr.count < count {
+                arr.append(matrix[bottom][i])
+            }
+            bottom -= 1
+            for i in stride(from: bottom, to: top-1, by: -1) where arr.count < count {
+                arr.append(matrix[i][left])
+            }
+            left += 1
+        }
+        return arr
+    }
+    
+    static func generate(_ numRows: Int) -> [[Int]] {
+        if numRows == 1 { return [[1]] }
+        if numRows == 2 { return [[1], [1, 1]]}
+        
+        var triangle = [[1]]
+        
+        for row in 1..<numRows {
+            var numRow = [1]
+            for column in 1..<row {
+                print(row, column)
+                numRow.append(triangle[row - 1][column - 1] + triangle[row - 1][column])
+            }
+            numRow.append(1)
+            triangle.append(numRow)
+        }
+        
+        return triangle
+    }
+}
+
+print(Solution.pivotIndex([1,7,3,6,5,6]))
+print(Solution.pivotIndex([1,2,3]))
+print(Solution.pivotIndex([2,1,-1]))
+print(Solution.pivotIndex([0]))
+print(Solution.dominantIndex([3,6,1,0]))
+print(Solution.dominantIndex([1,2,3,4]))
+print(Solution.plusOne([9]))
+print(Solution.plusOne([4,3,2,1]))
+print(Solution.plusOne([1,2,3]))
+print(Solution.findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(Solution.findDiagonalOrder([[1,2],[3,4]]))
+print(Solution.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(Solution.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+print(Solution.generate(5))
+print(Solution.generate(1))
---
+++ Source/Day6/README.md
@@ -0,0 +1,123 @@
+#  [Day 6 - Array and String: Introduction (pt. 1)](https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/)
+
+## 1. Find Pivot Index
+
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+```
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+```
+
+```
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+```
+
+```
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+```
+
+## 2. Largest Number At Least Twice of Others
+
+You are given an integer array nums where the largest integer is unique.
+
+Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+
+```
+Input: nums = [3,6,1,0]
+Output: 1
+Explanation: 6 is the largest integer.
+For every other number in the array x, 6 is at least twice as big as x.
+The index of value 6 is 1, so we return 1.
+```
+
+```
+Input: nums = [1,2,3,4]
+Output: -1
+Explanation: 4 is less than twice the value of 3, so we return -1.
+```
+
+## 3. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+```
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+```
+
+```
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+```
+
+```
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+```
+
+## 4. Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+
+```
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,4,7,5,3,6,8,9]
+```
+
+```
+Input: mat = [[1,2],[3,4]]
+Output: [1,2,3,4]
+```
+
+## 5. Given an m x n matrix, return all elements of the matrix in spiral order.
+
+```
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+```
+
+```
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+```
+
+## 6. Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+```
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+```
+
+```
+Input: numRows = 1
+Output: [[1]]
+```
