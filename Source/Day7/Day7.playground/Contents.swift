a806df39ef53c88e41f66e6cd2da72ee633d1b6a
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-18 11:46:00 +0800

    - add binary

--- Source/Day7/Day7.playground/Contents.swift
+++ Source/Day7/Day7.playground/Contents.swift
@@ -1,3 +1,50 @@
-import UIKit
+class Solution {
+    
+    // without casting to Int
+    static func addBinary(_ a: String, _ b: String) -> String {
+        var result = ""
+        var carry = 0
+        var a = a
+        var b = b
+        var i = a.count - 1
+        var j = b.count - 1
+        
+        while i >= 0 || j >= 0 {
+            let valA = i < 0 ? "0": a.popLast()!
+            let valB = j < 0 ? "0" : b.popLast()!
+            
+            switch (valA, valB) {
+            case ("1", "1") where carry == 1:
+                result.insert("1", at: result.startIndex)
+                carry = 1
+            case ("1", "1") where carry == 0,
+                ("1", "0") where carry == 1,
+                ("0", "1") where carry == 1 :
+                result.insert("0", at: result.startIndex)
+                carry = 1
+            case ("0", "0") where carry == 1:
+                result.insert("1", at: result.startIndex)
+                carry = 0
+            case ("0", "0"):
+                result.insert("0", at: result.startIndex)
+            case ("1", "0"), ("0", "1"):
+                result.insert("1", at: result.startIndex)
+            default:
+                continue
+            }
+            i -= 1
+            j -= 1
+        }
+        
+        if carry == 1 {
+            result.insert("1", at: result.startIndex)
+        }
+        
+        return result
+    }
+}
 
-var greeting = "Hello, playground"
+
+print(Solution.addBinary("11", "1"))
+print(Solution.addBinary("1010", "1011"))
+print(Solution.addBinary("1", "111"))
--- Source/Day7/README.md
+++ Source/Day7/README.md
@@ -1,2 +1,15 @@
-#  <#Title#>
+#  [Day 7 - Array and String: String (pt. 2)](https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/)
 
+## 1. Add Binary
+
+Given two binary strings a and b, return their sum as a binary string.
+
+```
+Input: a = "11", b = "1"
+Output: "100"
+```
+
+```
+Input: a = "1010", b = "1011"
+Output: "10101"
+```
