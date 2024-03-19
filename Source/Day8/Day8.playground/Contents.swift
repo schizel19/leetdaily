edcd4958f881573ffa06b6450d11cf817ca2cb7b
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-19 08:41:40 +0800

    - rotate array

--- Source/Day8/Day8.playground/Contents.swift
+++ Source/Day8/Day8.playground/Contents.swift
@@ -15,9 +15,45 @@ class Solution {
         
         return ans == Int.max ? 0 : ans
     }
+    
+    static func rotate(_ nums: inout [Int], _ k: Int) {
+        var k = k % nums.count
+        if k == 0 { return }
+        var left = 0, right = nums.count - 1
+        
+        while left < right {
+            nums.swapAt(left, right)
+            left += 1
+            right -= 1
+        }
+        
+        left = 0
+        right = k - 1
+        while left < right {
+            nums.swapAt(left, right)
+            left += 1
+            right -= 1
+        }
+        
+        left = k
+        right = nums.count - 1
+        while left < right {
+            nums.swapAt(left, right)
+            left += 1
+            right -= 1
+        }
+    }
 }
 
 print(Solution.minSubArrayLen(7, [2,3,1,2,4,3]))
 print(Solution.minSubArrayLen(4, [1,4,4]))
 print(Solution.minSubArrayLen(11, [1,1,1,1,1,1,1,1]))
 print(Solution.minSubArrayLen(213, [12,28,83,4,25,26,25,2,25,25,25,12]))
+
+var array = [1,2,3,4,5,6,7]
+Solution.rotate(&array, 3)
+print(array)
+
+var array2 = [-1,-100,3,99] // [3, 99, -1, 100]
+Solution.rotate(&array2, 2)
+print(array2)
--- Source/Day8/README.md
+++ Source/Day8/README.md
@@ -20,3 +20,24 @@ Output: 1
 Input: target = 11, nums = [1,1,1,1,1,1,1,1]
 Output: 0
 ```
+
+## 2. Rotate Array
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+```
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+```
+
+```
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+```
