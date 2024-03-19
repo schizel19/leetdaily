a4e34344fdfbb9029fa8d4e1416e9cffe152cf81
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-19 08:13:59 +0800

    - minimum size subarray sum

--- Source/Day8/Day8.playground/Contents.swift
+++ Source/Day8/Day8.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+class Solution {
+    // sliding window
+    static func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
+        var left = 0
+        var sum = 0
+        var ans = Int.max
+        for right in 0..<nums.count {
+            sum += nums[right]
+            while sum >= target {
+                ans = min(ans, right + 1 - left)
+                sum -= nums[left]
+                left += 1
+            }
+        }
+        
+        return ans == Int.max ? 0 : ans
+    }
+}
 
-var greeting = "Hello, playground"
+print(Solution.minSubArrayLen(7, [2,3,1,2,4,3]))
+print(Solution.minSubArrayLen(4, [1,4,4]))
+print(Solution.minSubArrayLen(11, [1,1,1,1,1,1,1,1]))
+print(Solution.minSubArrayLen(213, [12,28,83,4,25,26,25,2,25,25,25,12]))
--- Source/Day8/README.md
+++ Source/Day8/README.md
@@ -1,2 +1,22 @@
-#  <#Title#>
+#  [Day 8 - Array and String: Challenge (pt. 3)](https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/) 
 
+
+## 1. Minimum Size Subarray Sum
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+```
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+```
+
+```
+Input: target = 4, nums = [1,4,4]
+Output: 1
+```
+
+```
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+```
