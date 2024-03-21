13b3ad8b38c7a73038ca0bb2c55f661f00b8b3f1
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-21 14:14:09 +0800

    - reverse singly linked list

--- Source/Day10/Day10.playground/Contents.swift
+++ Source/Day10/Day10.playground/Contents.swift
@@ -1,3 +1,60 @@
-import UIKit
+class ListNode {
+     public var val: Int
+     public var next: ListNode?
+     public init(_ val: Int) {
+         self.val = val
+         self.next = nil
+     }
+}
 
-var greeting = "Hello, playground"
+class Solution {
+    static func reverseList(_ head: ListNode?) -> ListNode? {
+        var head = head
+        var newHead: ListNode?
+        
+        while head != nil {
+            let nextHead = head?.next
+            head?.next = newHead
+            newHead = head
+            head = nextHead
+        }
+        
+        return newHead
+    }
+    
+    static func list(head: ListNode?) {
+        var vals = [String]()
+        var head = head
+        while head != nil {
+            vals.append("\(head?.val)")
+            head = head?.next
+            
+            if vals.count == 6 {
+                print(vals)
+                break
+            }
+        }
+        print(vals.joined(separator: ", "))
+    }
+}
+
+let node1 = ListNode(1)
+let node2 = ListNode(2)
+let node3 = ListNode(3)
+let node4 = ListNode(4)
+let node5 = ListNode(5)
+let node6 = ListNode(6)
+let node7 = ListNode(7)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+Solution.list(head: Solution.reverseList(node1)) // 5, 4, 3, 2, 1
+
+node6.next = node7
+
+Solution.list(head: Solution.reverseList(node6)) // 7, 6
+
+Solution.list(head: Solution.reverseList(nil)) // nil
--- Source/Day10/README.md
+++ Source/Day10/README.md
@@ -1,2 +1,24 @@
-#  <#Title#>
+#  [Day 10 - Linked List: Classic Problems (pt. 2)](https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1204/)
 
+## 1. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+![Example 1:](https://assets.leetcode.com/uploads/2021/02/19/rev1ex1.jpg)
+
+```
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+```
+
+![Example 2:](https://assets.leetcode.com/uploads/2021/02/19/rev1ex2.jpg)
+
+```
+Input: head = [1,2]
+Output: [2,1]
+```
+
+```
+Input: head = []
+Output: []
+```
