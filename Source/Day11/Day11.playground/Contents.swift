89611d1b6fb2866c673a749e3d04f53a9e588ff4
Patrick <patrick.wittybunny@gmail.com>
2024-03-22 15:14:01 +0800

    Day 11 - Linked List (pt. 3) (#11)

---
+++ Source/Day11/Day11.playground/Contents.swift
@@ -0,0 +1,297 @@
+class ListNode {
+    public var val: Int
+    public var prev: ListNode?
+    public var next: ListNode?
+    public var child: ListNode?
+    
+    public init(_ val: Int, prev: ListNode? = nil, _ next: ListNode? = nil, child: ListNode? = nil) {
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+    }
+}
+
+func list(head: ListNode?) {
+    var vals = [String]()
+    var head = head
+    while head != nil {
+        vals.append("\(head?.val)")
+        head = head?.next
+        
+        if vals.count == 1000 {
+            print(vals)
+            break
+        }
+    }
+    print(vals.isEmpty ? "empty" : vals.joined(separator: ", "))
+}
+
+public class Node {
+    public var val: Int
+    public var next: Node?
+    public var random: Node?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+        self.random = nil
+    }
+}
+
+
+func list2(head: Node?) {
+    var vals = [String]()
+    var head = head
+    while head != nil {
+        vals.append("\(head?.val)")
+        head = head?.next
+        
+        if vals.count == 1000 {
+            print(vals)
+            break
+        }
+    }
+    print(vals.isEmpty ? "empty" : vals.joined(separator: ", "))
+}
+
+let node1 = ListNode(1)
+let node2 = ListNode(2)
+let node3 = ListNode(3)
+let node4 = ListNode(4)
+let node5 = ListNode(5)
+let node6 = ListNode(6)
+let node7 = ListNode(7)
+
+let nodeA = ListNode(8)
+let nodeB = ListNode(9)
+let nodeC = ListNode(10)
+let nodeD = ListNode(11)
+let nodeE = ListNode(12)
+let nodeF = ListNode(13)
+let nodeG = ListNode(14)
+let nodeH = ListNode(15)
+let nodeI = ListNode(16)
+let nodeJ = ListNode(17)
+let nodeK = ListNode(18)
+let nodeL = ListNode(19)
+
+
+class Solution {
+    static func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        let parent = ListNode(-1)
+        var head: ListNode? = parent
+        var l1 = list1
+        var l2 = list2
+        
+        while l1 != nil || l2 != nil {
+            var next: ListNode?
+            
+            if let val1 = l1?.val, let val2 = l2?.val {
+                if val1 < val2 {
+                    next = l1
+                    l1 = l1?.next
+                } else {
+                    next = l2
+                    l2 = l2?.next
+                }
+            } else if l1 != nil {
+                next = l1
+                l1 = l1?.next
+            } else {
+                next = l2
+                l2 = l2?.next
+            }
+            
+            
+            head?.next = next
+            head = next
+        }
+        
+        return parent.next
+    }
+    
+    static func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        let parent = ListNode(0)
+        var carry = 0
+        var head = parent
+        var l1 = l1
+        var l2 = l2
+        
+        while l1 != nil || l2 != nil {
+            let sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry
+            let node = ListNode(sum % 10)
+            carry = sum/10
+            l1 = l1?.next
+            l2 = l2?.next
+            head.next = node
+            head = node
+        }
+        
+        if carry == 1 {
+            head.next = ListNode(1)
+        }
+        
+        return parent.next
+    }
+    
+    static func flatten(_ head: ListNode?) -> ListNode? {
+        let parent = ListNode(0, head)
+        var head = head
+        var next: ListNode?
+        
+        while head != nil {
+            if head?.child != nil {
+                next = head?.next
+                head?.next = flatten(head?.child)
+                head?.next?.prev = head
+                head?.child = nil
+                while head?.next != nil {
+                    head = head?.next
+                }
+                head?.next = next
+                next?.prev = head
+            } else {
+                head = head?.next
+            }
+        }
+        parent.next?.prev = nil
+        return parent.next
+    }
+    
+    static func copyRandomList(_ head: Node?) -> Node? {
+        if head == nil { return nil }
+        var start = Node(0)
+        var oldHead = head
+        
+        while oldHead != nil {
+            let node = Node(oldHead!.val)
+            node.next = oldHead?.next
+            oldHead?.next = node
+            oldHead = node.next
+        }
+        
+        oldHead = head
+        while oldHead != nil {
+            oldHead?.next?.random = oldHead?.random?.next
+            oldHead = oldHead?.next?.next
+        }
+        
+        oldHead = head
+        var newHead: Node? = head?.next
+        var newNode = oldHead?.next
+        
+        while oldHead != nil {
+            oldHead?.next = newNode?.next
+            oldHead = oldHead?.next
+            newNode?.next = oldHead?.next
+            newNode = newNode?.next
+        }
+        
+        return newHead
+    }
+    
+    static func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        if head == nil || k == 0 { return head }
+        var node = head
+        var count = 1
+        
+        while node?.next != nil {
+            node = node?.next
+            count += 1
+        }
+        
+        node?.next = head
+        
+        for _ in 0..<count - (k % count) {
+            node = node?.next
+        }
+        
+        var head = node?.next
+        node?.next = nil
+        return head
+    }
+}
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = nil
+
+// [1, 2, 3, 4, 5] -> [4, 5, 1, 2, 3]
+list(head: Solution.rotateRight(node1, 2))
+list(head: Solution.rotateRight(nil, 1))
+////
+node6.next = nil
+list(head: Solution.rotateRight(node6, 0))
+list(head: Solution.rotateRight(node6, 1))
+
+node1.next = node2
+node2.next = nil
+list(head: Solution.rotateRight(node1, 2))
+
+// [1, 2, 4, 5] + [3, 6] = [1, 2, 3, 4, 5, 6]
+node1.next = node2
+node2.next = node4
+node4.next = node5
+
+node3.next = node6
+
+list(head: Solution.mergeTwoLists(node1, node3))
+
+
+// 315
+node5.next = node1
+node1.next = node3
+node3.next = nil
+
+// 724
+node4.next = node2
+node2.next = node7
+node7.next = nil
+
+// 724 + 315 = 1039
+list(head: Solution.addTwoNumbers(node5, node4))
+
+
+nodeA.next = nodeB
+nodeB.next = nodeC
+nodeC.next = nodeD
+nodeD.child = nodeE
+nodeE.next = nodeF
+nodeF.next = nodeG
+nodeG.child = nodeH
+nodeH.child = nodeI
+nodeH.next = nodeJ
+nodeG.next = nodeK
+nodeD.next = nodeL
+
+/*
+A B C D L
+      E F G K
+          H J
+          I
+ 
+ output: A, B, C, D, E, F, G, H, I, J, K, L
+*/
+
+list(head: Solution.flatten(nodeA))
+
+let aNode7 = Node(7)
+let aNode13 = Node(13)
+let aNode11 = Node(11)
+let aNode10 = Node(10)
+let aNode1 = Node(1)
+
+aNode7.next = aNode13
+aNode13.next = aNode11
+aNode11.next = aNode10
+aNode10.next = aNode1
+
+aNode13.random = aNode7
+aNode11.random = aNode10
+aNode10.random = aNode13
+aNode1.random = aNode7
+
+list2(head: Solution.copyRandomList(aNode7))
+
---
+++ Source/Day11/README.md
@@ -0,0 +1,142 @@
+#  [Day 11 - Linked List: Challenge (pt. 3)](https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1227/)
+
+
+## 1. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
+![Example 1:](https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg)
+
+```
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+```
+
+```
+Input: list1 = [], list2 = []
+Output: []
+```
+
+```
+Input: list1 = [], list2 = [0]
+Output: [0]
+```
+
+## 2. Add Two Numbers
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+![Example 1:](https://assets.leetcode.com/uploads/2020/10/02/addtwonumber1.jpg)
+
+```
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+```
+
+```
+Input: l1 = [0], l2 = [0]
+Output: [0]
+```
+
+```
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+```
+
+## 3. Flatten a Multilevel Doubly Linked List
+
+You are given a doubly linked list, which contains nodes that have a next pointer, a previous pointer, and an additional child pointer. This child pointer may or may not point to a separate doubly linked list, also containing these special nodes. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure as shown in the example below.
+
+Given the head of the first level of the list, flatten the list so that all the nodes appear in a single-level, doubly linked list. Let curr be a node with a child list. The nodes in the child list should appear after curr and before curr.next in the flattened list.
+
+Return the head of the flattened list. The nodes in the list must have all of their child pointers set to null.
+
+Example 1:
+![Example 1:](https://assets.leetcode.com/uploads/2021/11/09/flatten11.jpg)
+
+```
+Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+Explanation: The multilevel linked list in the input is shown.
+After flattening the multilevel linked list it becomes:
+```
+
+![Result 1:](https://assets.leetcode.com/uploads/2021/11/09/flatten12.jpg)
+
+Example 2:
+![Example 2:](https://assets.leetcode.com/uploads/2021/11/09/flatten2.1jpg)
+
+```
+Input: head = [1,2,null,3]
+Output: [1,3,2]
+Explanation: The multilevel linked list in the input is shown.
+After flattening the multilevel linked list it becomes:
+```
+
+![Result 3:](https://assets.leetcode.com/uploads/2021/11/24/list.jpg)
+
+## 4. Copy List with Random Pointer
+
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+
+For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+
+Return the head of the copied linked list.
+
+The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+Your code will only be given the head of the original linked list.
+
+Example 1:
+![Example 1:](https://assets.leetcode.com/uploads/2019/12/18/e1.png)
+
+```
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+```
+
+Example 2:
+![Example 2:](https://assets.leetcode.com/uploads/2019/12/18/e2.png)
+
+```
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+```
+
+Example 3:
+![Example 3:](https://assets.leetcode.com/uploads/2019/12/18/e3.png)
+
+```
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+```
+
+## 5. Rotate List by k places
+
+Given the head of a linked list, rotate the list to the right by k places.
+
+Example 1:
+![Example 1:](https://assets.leetcode.com/uploads/2020/11/13/rotate1.jpg)
+```
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+```
+
+Example 2:
+![Example 2:](https://assets.leetcode.com/uploads/2020/11/13/roate2.jpg)
+```
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+```
