4f03fd6e26dd27fcdaed16b496dc672b733eb94b
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-22 10:35:10 +0800

    - flatten multilevel doubly linked list

--- Source/Day11/Day11.playground/Contents.swift
+++ Source/Day11/Day11.playground/Contents.swift
@@ -2,11 +2,13 @@ class ListNode {
     public var val: Int
     public var prev: ListNode?
     public var next: ListNode?
+    public var child: ListNode?
     
-    public init(_ val: Int, _ prev: ListNode? = nil, _ next: ListNode? = nil) {
+    public init(_ val: Int, prev: ListNode? = nil, _ next: ListNode? = nil, child: ListNode? = nil) {
         self.val = val
         self.prev = prev
         self.next = next
+        self.child = child
     }
 }
 
@@ -39,6 +41,12 @@ let nodeC = ListNode(10)
 let nodeD = ListNode(11)
 let nodeE = ListNode(12)
 let nodeF = ListNode(13)
+let nodeG = ListNode(14)
+let nodeH = ListNode(15)
+let nodeI = ListNode(16)
+let nodeJ = ListNode(17)
+let nodeK = ListNode(18)
+let nodeL = ListNode(19)
 
 
 class Solution {
@@ -98,6 +106,30 @@ class Solution {
         
         return parent.next
     }
+    
+    static func flatten(_ head: ListNode?) -> ListNode? {
+        let parent = ListNode(0, head)
+        var head = head
+        var next: ListNode?
+        
+        while head != nil {
+            if head?.child != nil {
+                next = head?.next
+                head?.next = flatten(head?.child)
+                head?.next?.prev = head
+                head?.child = nil
+                while head?.next != nil {
+                    head = head?.next
+                }
+                head?.next = next
+                next?.prev = head
+            } else {
+                head = head?.next
+            }
+        }
+        parent.next?.prev = nil
+        return parent.next
+    }
 }
 
 // [1, 2, 4, 5] + [3, 6] = [1, 2, 3, 4, 5, 6]
@@ -122,3 +154,27 @@ node7.next = nil
 
 // 724 + 315 = 1039
 list(head: Solution.addTwoNumbers(node5, node4))
+
+
+nodeA.next = nodeB
+nodeB.next = nodeC
+nodeC.next = nodeD
+nodeD.child = nodeE
+nodeE.next = nodeF
+nodeF.next = nodeG
+nodeG.child = nodeH
+nodeH.child = nodeI
+nodeH.next = nodeJ
+nodeG.next = nodeK
+nodeD.next = nodeL
+
+/*
+A B C D L
+      E F G K
+          H J
+          I
+ 
+ output: A, B, C, D, E, F, G, H, I, J, K, L
+*/
+
+list(head: Solution.flatten(nodeA))
