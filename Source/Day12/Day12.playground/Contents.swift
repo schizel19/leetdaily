51215cf3cece4a82bdb02ca73fdb946b87334371
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-03-23 14:03:19 +0800

    - swap nodes in pairs

--- Source/Day12/Day12.playground/Contents.swift
+++ Source/Day12/Day12.playground/Contents.swift
@@ -1,3 +1,52 @@
-import UIKit
 
-var greeting = "Hello, playground"
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+func list(head: ListNode?) {
+    var vals = [String]()
+    var head = head
+    while head != nil {
+        vals.append("\(head?.val)")
+        head = head?.next
+        
+        if vals.count == 1000 {
+            print(vals)
+            break
+        }
+    }
+    print(vals.isEmpty ? "empty" : vals.joined(separator: ", "))
+}
+
+let node1 = ListNode(1)
+let node2 = ListNode(2)
+let node3 = ListNode(3)
+let node4 = ListNode(4)
+
+class Solution {
+    static func swapPairs(_ head: ListNode?) -> ListNode? {
+        if head == nil || head?.next == nil { return head }
+        var sentry = ListNode(0, head?.next)
+        let next = head?.next?.next
+        head?.next?.next = head
+        head?.next = swapPairs(next)
+        return sentry.next
+    }
+}
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = nil
+
+list(head: Solution.swapPairs(node1))
+
+node1.next = nil
+
+list(head: Solution.swapPairs(node1))
+
+
--- Source/Day12/README.md
+++ Source/Day12/README.md
@@ -1,2 +1,23 @@
-#  <#Title#>
+#  [Day 12 - Recursion I: Recurence and Memoization (pt.1)](https://leetcode.com/explore/learn/card/recursion-i/)
 
+## 1. Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Example 1:
+![Example 1:](https://assets.leetcode.com/uploads/2020/10/03/swap_ex1.jpg)
+
+```
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+```
+
+```
+Input: head = []
+Output: []
+```
+
+```
+Input: head = [1]
+Output: [1]
+```
