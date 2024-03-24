bbd437dc315f4c3d8385ae9e6256639f8bca4924
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-03-24 13:48:41 +0800

    - max depth of binary tree

--- Source/Day13/Day13.playground/Contents.swift
+++ Source/Day13/Day13.playground/Contents.swift
@@ -1,1 +1,33 @@
 
+public class TreeNode {
+  public var val: Int
+  public var left: TreeNode?
+  public var right: TreeNode?
+  public init() { self.val = 0; self.left = nil; self.right = nil; }
+  public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+  public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+      self.val = val
+      self.left = left
+      self.right = right
+  }
+}
+
+class Solution {
+    
+    func maxDepth(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
+    }
+}
+
+
+let node1 = TreeNode(1)
+let node2 = TreeNode(2)
+let node3 = TreeNode(3)
+let node4 = TreeNode(4)
+
+node1.left = node2
+node1.right = node3
+node3.right = node4
+
+print(Solution().maxDepth(node1))
--- Source/Day13/README.md
+++ Source/Day13/README.md
@@ -1,2 +1,20 @@
-#  <#Title#>
+#  [Day 13 - Recursion I: Challenge (pt.2)](https://leetcode.com/explore/learn/card/recursion-i/256/complexity-analysis/)
+
+## 1. Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+![Image](https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg)
+
+```
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+```
+
+
+
+
 
