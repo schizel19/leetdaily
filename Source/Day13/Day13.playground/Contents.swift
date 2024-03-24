6471f332d37b00e85048ded72f5430df902c2c83
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-03-24 14:20:58 +0800

    - merge two sorted lists

--- Source/Day13/Day13.playground/Contents.swift
+++ Source/Day13/Day13.playground/Contents.swift
@@ -12,6 +12,28 @@ public class TreeNode {
   }
 }
 
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    public func list() {
+        var vals = [String]()
+        var head: ListNode? = self
+        while head != nil {
+            vals.append("\(head?.val)")
+            head = head?.next
+            if vals.count == 1000 {
+                print(vals)
+                break
+            }
+        }
+        print(vals.isEmpty ? "empty" : vals.joined(separator: ", "))
+    }
+}
+
 class Solution {
     
     func maxDepth(_ root: TreeNode?) -> Int {
@@ -23,6 +45,18 @@ class Solution {
         if n == 0 { return 1 }
         if n < 0 { return myPow(1/x, -n) }
         return n % 2 == 0 ? myPow(x * x, n/2) : x * myPow(x * x, n/2)
+    }
+    
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        if list1 == nil { return list2 }
+        if list2 == nil { return list1 }
+        if list1!.val < list2!.val {
+            list1?.next = mergeTwoLists(list1?.next, list2)
+            return list1
+        } else {
+            list2?.next = mergeTwoLists(list1, list2?.next)
+            return list2
+        }
     }
 }
 
@@ -41,3 +75,20 @@ print(Solution().myPow(2.0, 10))
 print(Solution().myPow(2.1, 3))
 print(Solution().myPow(2.0, -2))
 print(Solution().myPow(0.00001, 2147483647))
+
+
+let nodeA = ListNode(1)
+let nodeB = ListNode(2)
+let nodeC = ListNode(4)
+let nodeD = ListNode(1)
+let nodeE = ListNode(3)
+let nodeF = ListNode(4)
+
+nodeA.next = nodeB
+nodeB.next = nodeC
+
+nodeD.next = nodeE
+nodeE.next = nodeF
+
+// 1, 2, 4 + 1, 3, 4 = 1, 1, 2, 3, 4, 4
+Solution().mergeTwoLists(nodeA, nodeD)?.list()
--- Source/Day13/README.md
+++ Source/Day13/README.md
@@ -28,4 +28,20 @@ Input: x = 2.10000, n = 3
 Output: 9.26100
 ```
 
+## 3. Merge two sorted lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+![Example:](https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg)
+
+```
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+```
+
 
