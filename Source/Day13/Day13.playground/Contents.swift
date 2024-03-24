1bcaf80e9c4da69366f426349a2db1e34887998e
Patrick <patrickleinieldomingo@gmail.com>
2024-03-24 15:08:27 +0800

    Day 13 - Recursion I (pt. 2) (#13)

---
+++ Source/Day13/Day13.playground/Contents.swift
@@ -0,0 +1,150 @@
+import Foundation
+
+public class TreeNode {
+  public var val: Int
+  public var left: TreeNode?
+  public var right: TreeNode?
+  public init() { self.val = 0; self.left = nil; self.right = nil; }
+  public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+  public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+      self.val = val
+      self.left = left
+      self.right = right
+  }
+}
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    public func list() {
+        var vals = [String]()
+        var head: ListNode? = self
+        while head != nil {
+            vals.append("\(head?.val)")
+            head = head?.next
+            if vals.count == 1000 {
+                print(vals)
+                break
+            }
+        }
+        print(vals.isEmpty ? "empty" : vals.joined(separator: ", "))
+    }
+}
+
+class Solution {
+    
+    func maxDepth(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
+    }
+    
+    func myPow(_ x: Double, _ n: Int) -> Double {
+        if n == 0 { return 1 }
+        if n < 0 { return myPow(1/x, -n) }
+        return n % 2 == 0 ? myPow(x * x, n/2) : x * myPow(x * x, n/2)
+    }
+    
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        if list1 == nil { return list2 }
+        if list2 == nil { return list1 }
+        if list1!.val < list2!.val {
+            list1?.next = mergeTwoLists(list1?.next, list2)
+            return list1
+        } else {
+            list2?.next = mergeTwoLists(list1, list2?.next)
+            return list2
+        }
+    }
+    
+    func kthGrammar(_ n: Int, _ k: Int) -> Int {
+        if n == 1 { return 0 }
+        let totalElements = Int(pow(2.0, Double(n - 1)))
+        let halfElements = totalElements/2
+        
+        if k > halfElements {
+            return 1 - kthGrammar(n, k - halfElements)
+        }
+        
+        return kthGrammar(n - 1, k)
+    }
+    
+    func generateTrees(_ n: Int) -> [TreeNode?] {
+        struct Pair: Hashable {
+            let a: Int, b: Int
+        }
+        
+        var memo = [Pair: [TreeNode?]]()
+        
+        func allPossible(start: Int, end: Int) -> [TreeNode?] {
+            var res = [TreeNode?]()
+            if start > end {
+                res.append(nil)
+                return res
+            }
+            
+            if let m = memo[.init(a: start, b: end)] {
+                return m
+            }
+            
+            for i in start..<end+1 {
+                var leftSubtrees = allPossible(start: start, end: i - 1)
+                var rightSubtrees = allPossible(start: i + 1, end: end)
+                
+                
+                for left in leftSubtrees {
+                    for right in rightSubtrees {
+                        let root = TreeNode(i, left, right)
+                        res.append(root)
+                    }
+                }
+            }
+            
+            memo[.init(a: start, b: end)] = res
+            return res
+        }
+        
+        return allPossible(start: 1, end: n)
+    }
+}
+
+
+let node1 = TreeNode(1)
+let node2 = TreeNode(2)
+let node3 = TreeNode(3)
+let node4 = TreeNode(4)
+
+node1.left = node2
+node1.right = node3
+node3.right = node4
+
+print(Solution().maxDepth(node1))
+print(Solution().myPow(2.0, 10))
+print(Solution().myPow(2.1, 3))
+print(Solution().myPow(2.0, -2))
+print(Solution().myPow(0.00001, 2147483647))
+
+
+let nodeA = ListNode(1)
+let nodeB = ListNode(2)
+let nodeC = ListNode(4)
+let nodeD = ListNode(1)
+let nodeE = ListNode(3)
+let nodeF = ListNode(4)
+
+nodeA.next = nodeB
+nodeB.next = nodeC
+
+nodeD.next = nodeE
+nodeE.next = nodeF
+
+// 1, 2, 4 + 1, 3, 4 = 1, 1, 2, 3, 4, 4
+Solution().mergeTwoLists(nodeA, nodeD)?.list()
+
+
+print(Solution().kthGrammar(1, 1))
+print(Solution().kthGrammar(2, 1))
+print(Solution().kthGrammar(2, 2))
---
+++ Source/Day13/README.md
@@ -0,0 +1,93 @@
+#  [Day 13 - Recursion I: Challenge (pt.2)](https://leetcode.com/explore/learn/card/recursion-i/256/complexity-analysis/)
+
+## 1. Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+![Image](https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg)
+
+```
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+```
+
+## 2. Pow(x,n)
+
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+```
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+```
+
+```
+Input: x = 2.10000, n = 3
+Output: 9.26100
+```
+
+## 3. Merge two sorted lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+![Example:](https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg)
+
+```
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+```
+
+## 4. K-th Symbol in Grammar
+
+We build a table of n rows (1-indexed). We start by writing 0 in the 1st row. Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
+
+For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 0110.
+Given two integer n and k, return the kth (1-indexed) symbol in the nth row of a table of n rows.
+
+```
+Input: n = 1, k = 1
+Output: 0
+Explanation: row 1: 0
+```
+
+```
+Input: n = 2, k = 1
+Output: 0
+Explanation: 
+row 1: 0
+row 2: 01
+```
+
+```
+Input: n = 2, k = 2
+Output: 1
+Explanation: 
+row 1: 0
+row 2: 01
+```
+
+## 5. Unique Binary Search Trees II
+
+Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+
+Example:
+![Example:](https://assets.leetcode.com/uploads/2021/01/18/uniquebstn3.jpg)
+
+```
+Input: n = 3
+Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+```
+
+```
+Input: n = 1
+Output: [[1]]
+```
+
+
