db6b36615e7e22ec81be9f124e0eb51c3daed9c2
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-25 13:08:19 +0800

    - design hashmap

--- Source/Day14/Day14.playground/Contents.swift
+++ Source/Day14/Day14.playground/Contents.swift
@@ -1,6 +1,6 @@
 class MyHashSet {
-    let size = 1000
-    var array = [[Int]](repeating: .init(), count: 1000)
+    let size = 10000
+    var array = [[Int]](repeating: .init(), count: 10000)
     
     init() {}
     
@@ -17,6 +17,37 @@ class MyHashSet {
     }
 }
 
+class MyHashMap {
+    struct Data {
+        let key: Int
+        let val: Int
+    }
+    
+    let size = 1000
+    var array = [[Data]](repeating: .init(), count: 1000)
+
+    
+    init() {
+        
+    }
+    
+    func put(_ key: Int, _ value: Int) {
+        remove(key)
+        array[key % size].append(.init(key: key, val: value))
+    }
+    
+    func get(_ key: Int) -> Int {
+        return array[key % size].first(where: { $0.key == key })?.val ?? -1
+    }
+    
+    func remove(_ key: Int) {
+        if var index = array[key % size].firstIndex(where:  { $0.key == key }) {
+            array[key % size].remove(at: index)
+        }
+    }
+}
+
+
 let obj = MyHashSet()
 obj.add(1)
 obj.remove(2)
@@ -30,3 +61,11 @@ print(obj.contains(1))
 obj.add(1000000)
 obj.add(1000000)
 print(obj.contains(0))
+
+
+let obj2 = MyHashMap()
+obj2.put(1, 1)
+let ret_2: Int = obj2.get(1)
+obj2.remove(1)
+print(ret_2)
+obj2.put(1000000, 1)
