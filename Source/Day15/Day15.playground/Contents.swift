438f5e66e96f35ba860c0c649e7e09bf9569e864
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-26 11:29:47 +0800

    - isomorphic strings

--- Source/Day15/Day15.playground/Contents.swift
+++ Source/Day15/Day15.playground/Contents.swift
@@ -12,8 +12,38 @@ class Solution {
         
         return []
     }
+    
+    func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else { return false }
+        var map = [Character: Character]()
+        var set = Set<Character>()
+        var accumulated = ""
+        
+        for index in s.indices {
+            if let newC = map[s[index]] {
+                accumulated += "\(newC)"
+                continue
+            }
+            
+            if set.contains(t[index]) {
+                return false
+            }
+            
+            accumulated += "\(t[index])"
+            map[s[index]] = t[index]
+            set.insert(t[index])
+        }
+        
+        return t == accumulated
+    }
+    
 }
 
-print(Solution().twoSum([2, 7, 11, 15], 9))
-print(Solution().twoSum([6, 5, 7, 8, 9, 3], 10))
-print(Solution().twoSum([2, 4, 11, 3], 6))
+print(Solution().twoSum([2, 7, 11, 15], 9)) // [0, 1]
+print(Solution().twoSum([6, 5, 7, 8, 9, 3], 10)) // [2, 5]
+print(Solution().twoSum([2, 4, 11, 3], 6)) // [0, 1]
+
+print(Solution().isIsomorphic("egg", "add")) // true
+print(Solution().isIsomorphic("foo", "bar")) // false
+print(Solution().isIsomorphic("paper", "title")) // true
+print(Solution().isIsomorphic("badc", "baba")) // true
--- Source/Day15/README.md
+++ Source/Day15/README.md
@@ -15,3 +15,26 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 ```
+
+## 2. Isomorphic Strings
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+```
+Input: s = "egg", t = "add"
+Output: true
+```
+
+```
+Input: s = "foo", t = "bar"
+Output: false
+```
+
+```
+Input: s = "paper", t = "title"
+Output: true
+```
