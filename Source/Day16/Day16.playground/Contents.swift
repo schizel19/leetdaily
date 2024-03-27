90896f6d9e517810c9c8cc300e2efbcfbe9bcb82
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-03-27 08:56:01 +0800

    - group anagrams

--- Source/Day16/Day16.playground/Contents.swift
+++ Source/Day16/Day16.playground/Contents.swift
@@ -1,3 +1,16 @@
 class Solution {
-    
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        if strs.count == 1 { return [strs] }
+        var anagrams = [String: [String]]()
+        
+        for str in strs {
+            anagrams["\(str.sorted())"] = anagrams["\(str.sorted())", default: []] + [str]
+        }
+        
+        return anagrams.map { $0.value }
+    }
 }
+
+print(Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"])) //[["bat"],["nat","tan"],["ate","eat","tea"]]
+print(Solution().groupAnagrams([""])) //[[""]]
+print(Solution().groupAnagrams(["a"])) // [["a"]]
--- Source/Day16/README.md
+++ Source/Day16/README.md
@@ -1,2 +1,8 @@
 # [Day 16 - Hash Table: Keys (pt. 3)](https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1121/)
 
+## 1. Group Anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
