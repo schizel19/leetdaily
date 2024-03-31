36ddfe3f5d5ce00f3a25234361b23ad390d7868d
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-03-31 18:12:54 +0800

    - clone graph

--- Source/Day20/Day20.playground/Contents.swift
+++ Source/Day20/Day20.playground/Contents.swift
@@ -48,3 +48,46 @@ let grid2: [[Character]] = [
 
 print(Solution().numIslands(grid1)) // 1
 print(Solution().numIslands(grid2)) // 2
+
+public class Node {
+    public var val: Int
+    public var neighbors: [Node?]
+    public init(_ val: Int) {
+        self.val = val
+        self.neighbors = []
+    }
+}
+
+class Solution2 {
+    func cloneGraph(_ node: Node?) -> Node? {
+        var map = [Int: Node?]()
+        
+        func dfs(_ node: Node?) -> Node? {
+            guard let node else { return nil }
+            if let n = map[node.val] {
+                return n
+            }
+            let n = Node(node.val)
+            map[node.val] = n
+            for neighbor in node.neighbors {
+                n.neighbors.append(dfs(neighbor))
+            }
+            return n
+        }
+        
+        return dfs(node)
+    }
+}
+
+let node1 = Node(1)
+let node2 = Node(2)
+let node3 = Node(3)
+let node4 = Node(4)
+
+node1.neighbors = [node2, node4]
+node2.neighbors = [node1, node3]
+node3.neighbors = [node2, node4]
+node4.neighbors = [node1, node3]
+
+print(Solution2().cloneGraph(node1) === node1) // false
+print(Solution2().cloneGraph(node1)!.val) // 1
