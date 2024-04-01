ca183a906388bb7968b05a8b53587c763b1704ee
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-04-01 10:13:11 +0800

    - implement queue using stack

--- Source/Day21/Day21.playground/Contents.swift
+++ Source/Day21/Day21.playground/Contents.swift
@@ -1,1 +1,41 @@
 
+class MyQueue {
+
+    private var stack1 = [Int]()
+    private var stack2 = [Int]()
+    private var front: Int!
+    init() {
+        
+    }
+    
+    func push(_ x: Int) {
+        if stack1.isEmpty { front = x }
+        stack1.append(x)
+    }
+    
+    func pop() -> Int {
+        if stack2.isEmpty {
+            while !stack1.isEmpty {
+                stack2.append(stack1.removeLast())
+            }
+        }
+        return stack2.removeLast()
+    }
+    
+    func peek() -> Int {
+        stack2.isEmpty ? front : stack2.last!
+    }
+    
+    func empty() -> Bool {
+        return stack1.isEmpty && stack2.isEmpty
+    }
+}
+
+let obj = MyQueue()
+obj.push(3)
+obj.push(5)
+print(obj.peek()) // 3
+print(obj.pop()) // 3
+print(obj.empty()) // false
+print(obj.pop()) // 5
+print(obj.empty()) // true
