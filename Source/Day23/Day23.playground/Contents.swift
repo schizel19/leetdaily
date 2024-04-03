81738bf4d856c07c74d3d819f62e42cc2eac58e1
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-04-03 09:45:57 +0800

    - symmetric tree recursive

--- Source/Day23/Day23.playground/Contents.swift
+++ Source/Day23/Day23.playground/Contents.swift
@@ -23,6 +23,46 @@ class Solution {
         if root == nil { return 0 }
         return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
     }
+    
+    // recursive
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        if root == nil { return true }
+        
+        func isMirror(_ node1: TreeNode?, _ node2: TreeNode?) -> Bool {
+            if node1 == nil, node2 == nil { return true }
+            if node1 == nil || node2 == nil { return false }
+            if node1!.val != node2!.val { return false }
+            return isMirror(node1?.left, node2?.right) && isMirror(node1?.right, node2?.left)
+        }
+        
+        return isMirror(root?.left, root?.right)
+    }
 }
 
 print(Solution().maxDepth(node1))
+
+let nodeA = TreeNode(1)
+let nodeB = TreeNode(2)
+let nodeC = TreeNode(3)
+let nodeD = TreeNode(4)
+let nodeF = TreeNode(2)
+let nodeG = TreeNode(3)
+let nodeH = TreeNode(4)
+
+nodeA.left = nodeB
+nodeA.right = nodeF
+nodeB.left = nodeC
+nodeB.right = nodeD
+nodeF.left = nodeH
+nodeF.right = nodeG
+
+print(Solution().isSymmetric(nodeA)) // true
+
+nodeA.left = nodeB
+nodeA.right = nodeF
+nodeB.left = nil
+nodeB.right = nodeC
+nodeF.left = nil
+nodeF.right = nodeG
+
+print(Solution().isSymmetric(nodeA)) // false
--- Source/Day23/README.md
+++ Source/Day23/README.md
@@ -25,3 +25,28 @@ Constraints:
 
 * The number of nodes in the tree is in the range [0, 104].
 * -100 <= Node.val <= 100
+
+## 2.  Symmetric Tree
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+![Example](https://assets.leetcode.com/uploads/2021/02/19/symtree1.jpg)
+
+```
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+```
+
+Example 2:
+![Example](https://assets.leetcode.com/uploads/2021/02/19/symtree2.jpg)
+```
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+```
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 1000].
+* -100 <= Node.val <= 100
+
+Follow up: Could you solve it both recursively and iteratively?
