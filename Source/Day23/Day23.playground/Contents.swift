9eae2149c7638d8b779b8ffe62b228e82ffc3c65
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-04-03 09:18:36 +0800

    - maximum depth of binary tree

--- Source/Day23/Day23.playground/Contents.swift
+++ Source/Day23/Day23.playground/Contents.swift
@@ -1,1 +1,28 @@
+public class TreeNode {
+  public var val: Int
+  public var left: TreeNode?
+  public var right: TreeNode?
+  public init() { self.val = 0; self.left = nil; self.right = nil; }
+  public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+  public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+      self.val = val
+      self.left = left
+      self.right = right
+  }
+}
 
+let node1 = TreeNode(1)
+let node2 = TreeNode(2)
+let node3 = TreeNode(3)
+
+node1.right = node2
+node2.left = node3
+
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
+    }
+}
+
+print(Solution().maxDepth(node1))
