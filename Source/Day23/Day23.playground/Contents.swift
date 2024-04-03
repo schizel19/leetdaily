b7915b04f42eff34a350fbbe5e355af1fae6a657
Patrick <patrick.wittybunny@gmail.com>
2024-04-03 15:59:08 +0800

    Day 23 - Binary Tree (pt. 2) (#23)

---
+++ Source/Day23/Day23.playground/Contents.swift
@@ -0,0 +1,204 @@
+public class TreeNode {
+  public var val: Int
+  public var left: TreeNode?
+  public var right: TreeNode?
+  public init() { self.val = 0; self.left = nil; self.right = nil; }
+  public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+  public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+      self.val = val
+      self.left = left
+      self.right = right
+  }
+}
+
+let node1 = TreeNode(1)
+let node2 = TreeNode(2)
+let node3 = TreeNode(3)
+
+node1.right = node2
+node2.left = node3
+
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
+    }
+    
+    // recursive
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        if root == nil { return true }
+        
+        func isMirror(_ node1: TreeNode?, _ node2: TreeNode?) -> Bool {
+            if node1 == nil, node2 == nil { return true }
+            if node1 == nil || node2 == nil { return false }
+            if node1!.val != node2!.val { return false }
+            return isMirror(node1?.left, node2?.right) && isMirror(node1?.right, node2?.left)
+        }
+        
+        return isMirror(root?.left, root?.right)
+    }
+    
+    // iterative
+    func isSymmetric2(_ root: TreeNode?) -> Bool {
+        var visitedLeft = Set<Int>()
+        var visitedRight = Set<Int>()
+        var queue = [(TreeNode, Bool)]()
+        
+        if root?.left == nil || root?.right == nil { return false }
+        
+        queue.append((root!.left!, false))
+        queue.append((root!.right!, true))
+        
+        while !queue.isEmpty {
+            let (curr, direction) = queue.removeFirst()
+            if direction == false {
+                if visitedRight.contains(curr.val) {
+                    visitedRight.remove(curr.val)
+                } else {
+                    visitedLeft.insert(curr.val)
+                }
+            } else {
+                if visitedLeft.contains(curr.val) {
+                    visitedLeft.remove(curr.val)
+                } else {
+                    visitedRight.insert(curr.val)
+                }
+            }
+            if curr.left != nil {
+                queue.append((curr.left!, false))
+            }
+            
+            if curr.right != nil {
+                queue.append((curr.right!, true))
+            }
+        }
+        
+        return visitedLeft.isEmpty && visitedRight.isEmpty
+    }
+    
+    func isSymmetric3(_ root: TreeNode?) -> Bool {
+        var stack = [root?.left, root?.right]
+        while !stack.isEmpty {
+            let node1 = stack.removeLast()
+            let node2 = stack.removeLast()
+            
+            if node1 == nil, node2 == nil, stack.isEmpty { break }
+            if node1 == nil, node2 == nil, !stack.isEmpty { continue }
+            if node1 == nil || node2 == nil { return false }
+            if node1!.val != node2!.val { return false }
+            stack.append(node1?.left)
+            stack.append(node2?.right)
+            stack.append(node1?.right)
+            stack.append(node2?.left)
+        }
+        
+        return true
+    }
+    
+    // iterative
+    func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+        guard let root else { return false }
+        var stack: [(TreeNode, Int)] = [(root, 0)]
+        
+        while !stack.isEmpty {
+            let (curr, sum) = stack.removeFirst()
+            if curr.left == nil, curr.right == nil, curr.val + sum == targetSum {
+                return true
+            }
+            if curr.left != nil {
+                stack.append((curr.left!, curr.val + sum))
+            }
+            if curr.right != nil {
+                stack.append((curr.right!, curr.val + sum))
+            }
+        }
+        
+        return false
+    }
+    
+    // recursive
+    func hasPathSum2(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+        guard let root else { return false }
+        if root.left == nil, root.right == nil, root.val == targetSum {
+            return true
+        }
+        return hasPathSum2(root.left, targetSum - root.val) || hasPathSum2(root.right, targetSum - root.val)
+    }
+    
+    func buildTree(_ inorder: [Int], _ postorder: [Int]) -> TreeNode? {
+        if inorder.isEmpty || postorder.isEmpty { return nil }
+        var postorder = postorder
+        
+        func helper(left: Int, right: Int) -> TreeNode? {
+            if left > right { return nil }
+            let root = TreeNode(postorder.removeLast())
+            let index = inorder.firstIndex(of: root.val)!
+            
+            root.right = helper(left: index + 1, right: right)
+            root.left = helper(left: left, right: index - 1)
+            
+            return root
+        }
+       
+        return helper(left: 0, right: inorder.count - 1)
+    }
+
+}
+
+print(Solution().maxDepth(node1))
+
+let nodeA = TreeNode(1)
+let nodeB = TreeNode(2)
+let nodeC = TreeNode(3)
+let nodeD = TreeNode(4)
+let nodeF = TreeNode(2)
+let nodeG = TreeNode(3)
+let nodeH = TreeNode(4)
+
+nodeA.left = nodeB
+nodeA.right = nodeF
+nodeB.left = nodeC
+nodeB.right = nodeD
+nodeF.left = nodeH
+nodeF.right = nodeG
+
+print(Solution().isSymmetric(nodeA)) // true
+print(Solution().isSymmetric2(nodeA)) // true
+print(Solution().isSymmetric3(nodeA)) // true
+
+nodeA.left = nodeB
+nodeA.right = nodeF
+nodeB.left = nil
+nodeB.right = nodeC
+nodeF.left = nil
+nodeF.right = nodeG
+
+print(Solution().isSymmetric(nodeA)) // false
+print(Solution().isSymmetric2(nodeA)) // false
+print(Solution().isSymmetric3(nodeA)) // false
+
+let nodeP9 = TreeNode(1)
+let nodeP8 = TreeNode(2)
+let nodeP7 = TreeNode(7)
+let nodeP6 = TreeNode(4, nil, nodeP9)
+let nodeP5 = TreeNode(13)
+let nodeP4 = TreeNode(11, nodeP7, nodeP8)
+let nodeP3 = TreeNode(8, nodeP5, nodeP6)
+let nodeP2 = TreeNode(4, nodeP4, nil)
+let nodeP1 = TreeNode(5, nodeP2, nodeP3)
+
+let nodeA3 = TreeNode(3)
+let nodeA2 = TreeNode(2)
+let nodeA1 = TreeNode(1, nodeA2, nodeA3)
+
+print(Solution().hasPathSum(nodeP1, 22)) // true
+print(Solution().hasPathSum(nodeA1, 5)) // false
+print(Solution().hasPathSum(nil, 0)) // false
+
+print(Solution().hasPathSum2(nodeP1, 22)) // true
+print(Solution().hasPathSum2(nodeA1, 5)) // false
+print(Solution().hasPathSum2(nil, 0)) // false
+
+
+let node = Solution().buildTree([9,3,15,20,7], [9,15,7,20,3]) // [3,9,20,null,null,15,7]
+print(node!.val) // 3
---
+++ Source/Day23/README.md
@@ -0,0 +1,118 @@
+#  [Day 23 - Binary Tree: Challenge (pt. 2)](https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/)
+
+## 1. Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+![Example](https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg)
+
+```
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+```
+
+Example 2:
+
+```
+Input: root = [1,null,2]
+Output: 2
+```
+
+Constraints:
+
+* The number of nodes in the tree is in the range [0, 104].
+* -100 <= Node.val <= 100
+
+## 2.  Symmetric Tree
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+![Example](https://assets.leetcode.com/uploads/2021/02/19/symtree1.jpg)
+
+```
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+```
+
+Example 2:
+![Example](https://assets.leetcode.com/uploads/2021/02/19/symtree2.jpg)
+```
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+```
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 1000].
+* -100 <= Node.val <= 100
+
+Follow up: Could you solve it both recursively and iteratively?
+
+## 3. Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+Example 1:
+![Example](https://assets.leetcode.com/uploads/2021/01/18/pathsum1.jpg)
+
+```
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+```
+
+Example 2:
+![Example](https://assets.leetcode.com/uploads/2021/01/18/pathsum2.jpg)
+
+```
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+```
+
+Example 3:
+
+```
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+``` 
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 5000].
+* -1000 <= Node.val <= 1000
+* -1000 <= targetSum <= 1000
+
+## 4. Construct Binary Tree from Inorder and Postorder Traversal
+
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+![Example](https://assets.leetcode.com/uploads/2021/02/19/tree.jpg)
+```
+Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+Output: [3,9,20,null,null,15,7]
+```
+
+Example 2:
+```
+Input: inorder = [-1], postorder = [-1]
+Output: [-1]
+```
+
+Constraints:
+* 1 <= inorder.length <= 3000
+* postorder.length == inorder.length
+* -3000 <= inorder[i], postorder[i] <= 3000
+* inorder and postorder consist of unique values.
+* Each value of postorder also appears in inorder.
+* inorder is guaranteed to be the inorder traversal of the tree.
+* postorder is guaranteed to be the postorder traversal of the tree.
