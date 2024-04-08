3f6ac23c5a7b4a076fede528ab918cae3a2e0388
patrick.wittybunny <patrick.wittybunny@gmail.com>
2024-04-08 09:27:56 +0800

    - map sum pairs

--- Source/Day28/Day28.playground/Contents.swift
+++ Source/Day28/Day28.playground/Contents.swift
@@ -57,3 +57,51 @@ print(trie.search("app")) // false
 print(trie.startsWith("app")) // true
 trie.insert("app")
 print(trie.search("app")) // true
+
+class MapSum {
+    class Node {
+        var children = [Character: Node]()
+        var score = 0
+    }
+    
+    private var sums = [String: Int]()
+    private var root = Node()
+    
+    init() {
+        
+    }
+    
+    func insert(_ key: String, _ val: Int) {
+        let delta = val - sums[key, default: 0]
+        sums[key] = val
+        
+        var current = root
+        current.score += delta
+        
+        for char in key {
+            if let node = current.children[char] {
+                current = node
+            } else {
+                let node = Node()
+                current.children[char] = node
+                current = node
+            }
+            current.score += delta
+        }
+    }
+    
+    func sum(_ prefix: String) -> Int {
+        var current = root
+        for char in prefix {
+            guard let node = current.children[char] else { return 0 }
+            current = node
+        }
+        return current.score
+    }
+}
+
+let mapSum = MapSum()
+mapSum.insert("apple", 3)
+mapSum.sum("ap") // 3
+mapSum.insert("app", 2)
+mapSum.sum("ap") // 5
