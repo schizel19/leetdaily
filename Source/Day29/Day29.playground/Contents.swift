316a9970e5a03876c643b5e33816d3d2ad0eca13
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-04-09 17:39:45 +0800

    - add and search word

--- Source/Day29/Day29.playground/Contents.swift
+++ Source/Day29/Day29.playground/Contents.swift
@@ -1,3 +1,67 @@
-import UIKit
 
-var greeting = "Hello, playground"
+class WordDictionary {
+    
+    class Node {
+        var isWord: Bool = false
+        var next = [Character: Node]()
+    }
+    
+    var root = Node()
+    
+    init() {
+        
+    }
+    
+    func addWord(_ word: String) {
+        var current = root
+        for char in word {
+            let node = current.next[char] ?? Node()
+            current.next[char] = node
+            current = node
+        }
+        current.isWord = true
+    }
+    
+    func search(_ word: String) -> Bool {
+        search(word, in: root)
+    }
+    
+    func search(_ word: String, in node: Node) -> Bool {
+        var current = node
+        for (index, char) in word.enumerated() {
+            if char == "." {
+                for key in current.next.keys {
+                    if search(String(Array(word)[index+1..<word.count]), in: current.next[key]!) {
+                        return true
+                    }
+                }
+                return false
+            }
+            if let next = current.next[char] {
+                current = next
+            } else {
+                return false
+            }
+        }
+        return current.isWord
+    }
+}
+
+let wordDictionary = WordDictionary()
+wordDictionary.addWord("bad")
+wordDictionary.addWord("dad")
+wordDictionary.addWord("mad")
+wordDictionary.search("pad") // false
+wordDictionary.search("bad") // true
+wordDictionary.search(".ad") // true
+wordDictionary.search("b..") // true
+
+let wordDictionary2 = WordDictionary()
+wordDictionary2.addWord("a")
+wordDictionary2.addWord("a")
+wordDictionary2.search(".")
+wordDictionary2.search("a")
+wordDictionary2.search("aa")
+
+
+
