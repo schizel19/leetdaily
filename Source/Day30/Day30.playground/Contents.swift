2524556c66bbe9f3497d62789a28dcc8a91cad1b
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-04-10 21:33:34 +0800

    - bst iterator

--- Source/Day30/Day30.playground/Contents.swift
+++ Source/Day30/Day30.playground/Contents.swift
@@ -10,3 +10,43 @@ public class TreeNode {
       self.right = right
   }
 }
+
+class BSTIterator {
+
+    private var node: TreeNode?
+    private var stack = [TreeNode?]()
+    init(_ root: TreeNode?) {
+        node = root
+    }
+    
+    func next() -> Int {
+        while node != nil {
+            stack.append(node)
+            node = node?.left
+        }
+        var n = stack.removeLast()
+        node = n?.right
+        return n!.val
+    }
+    
+    func hasNext() -> Bool {
+        node != nil || !stack.isEmpty
+    }
+}
+
+let nodeE = TreeNode(20)
+let nodeD = TreeNode(9)
+let nodeC = TreeNode(15, nodeD, nodeE)
+let nodeB = TreeNode(3)
+let nodeA = TreeNode(7, nodeB, nodeC)
+
+let bst = BSTIterator(nodeA)
+print(bst.next()) // 3
+print(bst.next()) // 7
+print(bst.hasNext()) // true
+print(bst.next()) // 9
+print(bst.hasNext()) // true
+print(bst.next()) // 15
+print(bst.hasNext()) // true
+print(bst.next()) // 20
+print(bst.hasNext()) // false
